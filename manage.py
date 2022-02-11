204455f75c05ea61512db6769edd418879d90beb
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-11 07:07:02 +0300

    setup test runner

--- manage.py
+++ manage.py
@@ -1,7 +1,23 @@
 from app import create_app
-
-
+from flask_migrate import Migrate
+import unittest
 app=create_app('development')
 
+migrate=Migrate()
+
+#migrate.init_app(app,db)
+
+@app.cli.command()
+def test():
+  '''
+  Run unit tests
+  '''
+  tests=unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
+
+@app.shell_context_processor
+def make_shell_context():
+  return dict(app=app)
+  
 
 
